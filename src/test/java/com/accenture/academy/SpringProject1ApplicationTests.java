07bfaafafe9bd62446752cb7f9f20a0a5bc0c236
Tomasz <wartego@wp.pl>
2024-01-15 10:26:35 +0100

    first commit

---
+++ src/test/java/com/accenture/academy/SpringProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
